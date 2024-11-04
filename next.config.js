ad014740e81b5ee0ea07bc47d803c91e7018717b
Matthias Meister <meister.matthias86@gmail.com>
2024-11-04 15:52:55 +0100

    next js 14

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js                                                     
+const nextConfig = {                                                  
+    images: {                                                           
+      domains: ['images.unsplash.com', 'placehold.co'],                 
+      dangerouslyAllowSVG: true, // Erlaube das Laden von SVG-Bildern   
+    },                                                                  
+  };                                                                    
+                                                                        
+  module.exports = nextConfig;  
